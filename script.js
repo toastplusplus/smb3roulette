3106f218b2a7440bc568bbb7d22da93ab3af85cb
sconger <scott.conger@gmail.com>
2025-01-20 15:59:25 -0800

    Something rendering

---
+++ script.js
@@ -0,0 +1,226 @@
+'use strict';
+
+const imageIds = [
+  'background',
+  'border',
+  'mushroom',
+  'flower',
+  'star'
+];
+const audioIds = [
+  'music_loop',
+  'one_up',
+  'match',
+  'no_match'
+];
+
+const pixelWidth = 234;
+const pixelHeight = 162;
+
+const imageMap = new Map();
+const audioMap = new Map();
+
+let pageVisible = document.visibilityState === 'visible';
+let loadedFiles = false;
+let started = false;
+let lastTimestamp = null;
+let currentGameHandler = null;
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+const box = canvas.getBoundingClientRect();
+let canvasWidth = box.width;
+let canvasHeight = box.height;
+
+// Music and image loading process
+let loadingPromises = [];
+for (const imageId of imageIds) {
+  let promise = new Promise((resolve, reject) => {
+    const imageElem = document.getElementById(imageId);
+    if (imageElem.complete) {
+      console.log(`Loaded image ${imageId} (loaded immediately)`);
+      resolve();
+    } else {
+      imageElem.addEventListener('load', () => {
+        console.log(`Loaded image ${imageId}`);
+        resolve();
+      });
+      imageElem.addEventListener('error', (err) => {
+        console.error(`Error loading image ${imageId}`);
+        reject();
+      });
+    }
+    imageMap.set(imageId, imageElem);
+  });
+  loadingPromises.push(promise);
+}
+
+for (const audioId of audioIds) {
+  let promise = new Promise((resolve, reject) => {
+    const audioElem = document.getElementById(audioId);
+    audioElem.addEventListener('loadeddata', () => {
+      console.log(`Loaded audio ${audioId}`);
+      resolve()
+    });
+    audioElem.addEventListener('error', (err) => {
+      console.error(`Error audio ${audioId}`);
+      reject();
+    });
+    audioMap.set(audioId, audioElem);
+  });
+  loadingPromises.push(promise);
+}
+
+Promise.all(loadingPromises).then(() => {
+  loadedFiles = true;
+  if (started) {
+    startup();
+  }
+});
+
+function startup() {
+  document.getElementById('pressSpace').style.display = 'none';
+  resizeCanvas();
+  window.requestAnimationFrame(gameRender);
+}
+
+function handleInteraction() {
+  if (!started) {
+    started = true;
+    if (loadedFiles) {
+      startup();
+    }
+  }
+}
+
+document.addEventListener('visibilitychange', function() {
+  pageVisible = document.visibilityState === 'visible';
+  if (pageVisible) {
+    console.log('Page visible');
+    if (started) {
+      window.requestAnimationFrame(gameRender);
+    }
+  } else {
+    console.log('Page not visible');
+    lastTimestamp = null;
+  }
+});
+
+document.addEventListener('click', function(event) {
+  handleInteraction();
+});
+
+document.addEventListener('keypress', function(event) {
+  if (event.code === 'Space') {
+    handleInteraction();
+  }
+});
+
+function resizeCanvas() {
+  const desiredAspectRatio = (13.0 / 9.0);
+  const actualAspectRatio = window.innerWidth / window.innerHeight;
+
+  if (actualAspectRatio > desiredAspectRatio) {
+    canvasWidth = window.innerHeight * (13.0 / 9.0);
+    canvasHeight = window.innerHeight;
+  } else if (actualAspectRatio < desiredAspectRatio) {
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerWidth * (9.0 / 13.0);
+  } else {
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+  }
+
+  canvasWidth = Math.floor(canvasWidth);
+  canvasHeight = Math.floor(canvasHeight);
+  canvas.setAttribute('width', `${canvasWidth}px`);
+  canvas.setAttribute('height', `${canvasHeight}px`);
+
+  // This seems to be need to reset whenever you resize in firefox
+  ctx.webkitImageSmoothingEnabled = false;
+  ctx.mozImageSmoothingEnabled = false;
+  ctx.imageSmoothingEnabled = false;
+}
+
+window.addEventListener('resize', function() {
+  if (started) {
+    resizeCanvas();
+  }
+});
+
+// Note that when rendering the sprites in pieces we need to be careful about pixel rounding, as
+// otherwise the browser will do it for us, and sometimes leave gaps.
+
+// Top part is tiles 1-2 out of 7 in game
+function paintTopPart(image, xOffset) {
+  const segmentSize = image.height / 7;
+  const canvasSegmentWidth = canvasWidth / 14;
+  const canvasSegmentHeight = canvasHeight / 9;
+  ctx.drawImage(image,
+    0, 0,
+    image.width, segmentSize * 2,
+    xOffset, Math.floor(canvasSegmentHeight),
+    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
+}
+
+// Middle part is tiles 3-5 out of 7 in game
+function paintMiddlePart(image, xOffset) {
+  const segmentSize = image.height / 7;
+  const canvasSegmentWidth = canvasWidth / 14;
+  const canvasSegmentHeight = canvasHeight / 9;
+  ctx.drawImage(image,
+    0, segmentSize * 2,
+    image.width, segmentSize * 3,
+    xOffset, Math.floor(canvasSegmentHeight * 3),
+    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 3));
+}
+
+// Bottom part is tiles 6-7 out of 7 in game
+function paintBottomPart(image, xOffset) {
+  const segmentSize = image.height / 7;
+  const canvasSegmentWidth = canvasWidth / 14;
+  const canvasSegmentHeight = canvasHeight / 9;
+  ctx.drawImage(image,
+    0, segmentSize * 5,
+    image.width, segmentSize * 2,
+    xOffset, Math.floor(canvasSegmentHeight * 6),
+    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
+}
+
+class InteractiveGameHandler {
+  render(_timePassed) {
+    ctx.fillStyle = '000';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  
+    const stage = imageMap.get('background');
+    ctx.drawImage(stage, 0, 0, canvasWidth, canvasHeight);
+
+    const star = imageMap.get('star');
+    paintTopPart(star, 0);
+    paintMiddlePart(star, 0);
+    paintBottomPart(star, 0);
+
+    const border = imageMap.get('border');
+    ctx.drawImage(border, 0, 0, canvasWidth, canvasHeight);
+  }
+
+  click(e) {
+  }
+}
+
+function gameRender(newTimestamp) {
+  // Calculate time passed
+  let timePassed = lastTimestamp ? (newTimestamp - lastTimestamp) : 0;
+  lastTimestamp = newTimestamp;
+
+  // Render
+  currentGameHandler.render(timePassed);
+
+  // Request next frame
+  if (pageVisible) {
+    window.requestAnimationFrame(gameRender);
+  }
+}
+
+currentGameHandler = new InteractiveGameHandler();
+resizeCanvas();
