97d6a037cdfaeb0f5e9b70e907402405aac3b644
sconger <scott.conger@gmail.com>
2025-01-20 16:39:29 -0800

    Progress

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Resource and loading data
 const imageIds = [
   'background',
   'border',
@@ -14,23 +15,42 @@ const audioIds = [
   'no_match'
 ];
 
-const pixelWidth = 234;
-const pixelHeight = 162;
-
 const imageMap = new Map();
 const audioMap = new Map();
 
 let pageVisible = document.visibilityState === 'visible';
 let loadedFiles = false;
 let started = false;
-let lastTimestamp = null;
-let currentGameHandler = null;
 
+// Canvas data
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
 const box = canvas.getBoundingClientRect();
 let canvasWidth = box.width;
 let canvasHeight = box.height;
+
+// Game state
+const STATE = {
+  ROW_SPIN: 'ROW_SPIN',
+  ROW_STOP: 'ROW_STOP',
+  PRIZE: 'PRIZE',
+};
+
+const PRIZES = ['5up', '2up', '3up', '2up'];
+const ROW_START_SPEEDS = [-1, 1, -1.1];
+const ROW_START_OFFSETS = [-64, -64, -64];
+
+const IMAGE_OFFSETS = [0, 128, 256, 384];
+const NEGATIVE_EDGE = -96;
+const POSITIVE_EDGE = 0;
+
+let lastTimestamp = null;
+let currentGameHandler = null;
+
+let gameState = STATE.ROW_SPIN;
+let rowSpeeds = ROW_START_SPEEDS.slice();
+let rowOffsets = ROW_START_OFFSETS.slice();
+let currentRowIndex = 0;
 
 // Music and image loading process
 let loadingPromises = [];
@@ -148,43 +168,39 @@ window.addEventListener('resize', function() {
   }
 });
 
-// Note that when rendering the sprites in pieces we need to be careful about pixel rounding, as
-// otherwise the browser will do it for us, and sometimes leave gaps.
-
-// Top part is tiles 1-2 out of 7 in game
-function paintTopPart(image, xOffset) {
+// RowIndex is the prize gmae row
+// Offset is in logical game pixels, (234 pixels across)
+function paintImageSegment(image, rowIndex, rowOffset) {
   const segmentSize = image.height / 7;
   const canvasSegmentWidth = canvasWidth / 14;
   const canvasSegmentHeight = canvasHeight / 9;
-  ctx.drawImage(image,
-    0, 0,
-    image.width, segmentSize * 2,
-    xOffset, Math.floor(canvasSegmentHeight),
-    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
-}
+  const canvasOffset = (canvasWidth / 234) * rowOffset;
 
-// Middle part is tiles 3-5 out of 7 in game
-function paintMiddlePart(image, xOffset) {
-  const segmentSize = image.height / 7;
-  const canvasSegmentWidth = canvasWidth / 14;
-  const canvasSegmentHeight = canvasHeight / 9;
-  ctx.drawImage(image,
-    0, segmentSize * 2,
-    image.width, segmentSize * 3,
-    xOffset, Math.floor(canvasSegmentHeight * 3),
-    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 3));
-}
+  // Note that when rendering the sprites in pieces we need to be careful about pixel rounding, as
+  // otherwise the browser will do it for us, and sometimes leave gaps.
 
-// Bottom part is tiles 6-7 out of 7 in game
-function paintBottomPart(image, xOffset) {
-  const segmentSize = image.height / 7;
-  const canvasSegmentWidth = canvasWidth / 14;
-  const canvasSegmentHeight = canvasHeight / 9;
-  ctx.drawImage(image,
-    0, segmentSize * 5,
-    image.width, segmentSize * 2,
-    xOffset, Math.floor(canvasSegmentHeight * 6),
-    canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
+  // Top part is tiles 1-2 out of 7 in game
+  // Middle part is tiles 3-5 out of 7 in game
+  // Bottom part is tiles 6-7 out of 7 in game
+  if (rowIndex === 0) {
+    ctx.drawImage(image,
+      0, 0,
+      image.width, segmentSize * 2,
+      canvasOffset, Math.floor(canvasSegmentHeight),
+      canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
+  } else if (rowIndex === 1) {
+    ctx.drawImage(image,
+      0, segmentSize * 2,
+      image.width, segmentSize * 3,
+      canvasOffset, Math.floor(canvasSegmentHeight * 3),
+      canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 3));
+  } else {
+    ctx.drawImage(image,
+      0, segmentSize * 5,
+      image.width, segmentSize * 2,
+      canvasOffset, Math.floor(canvasSegmentHeight * 6),
+      canvasSegmentWidth * 6, Math.ceil(canvasSegmentHeight * 2));
+  }
 }
 
 class InteractiveGameHandler {
@@ -195,10 +211,24 @@ class InteractiveGameHandler {
     const stage = imageMap.get('background');
     ctx.drawImage(stage, 0, 0, canvasWidth, canvasHeight);
 
+    const mushroom = imageMap.get('mushroom');
+    const flower = imageMap.get('flower');
     const star = imageMap.get('star');
-    paintTopPart(star, 0);
-    paintMiddlePart(star, 0);
-    paintBottomPart(star, 0);
+
+    const images = [
+      star,
+      mushroom,
+      flower,
+      mushroom
+    ];
+
+    for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+      const rowOffset = rowOffsets[imageIndex];
+      for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+        const imageOffset = IMAGE_OFFSETS[imageIndex];
+        paintImageSegment(images[imageIndex], rowIndex, rowOffset + imageOffset);
+      }
+    }
 
     const border = imageMap.get('border');
     ctx.drawImage(border, 0, 0, canvasWidth, canvasHeight);
